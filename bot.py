1261625cd638d36e23c75ac9a56c0abbc2a6e10b
Furkan Kalaycıoğlu <Furkan0Kalaycioglu@gmail.com>
2021-05-16 19:07:36 +0300

    New functions

--- bot.py
+++ bot.py
@@ -1,22 +1,50 @@
 import os
-from PIL import Image 
-import PIL
 import discord
 from dotenv import load_dotenv
 import requests
 import json
 
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+prefix='=<'
 client = discord.Client()
 baseulr="https://cdn.betterttv.net/emote/"
-
-
 with open('emotes.json') as f:
     bttvemotes = json.load(f)
 
+commands={'add','remove','emotelist'}
+async def add_emote(message):
+    if message.content.split()[2] in bttvemotes:
+        await message.channel.send("This emote already exists")
+    else:
+        #updatedic= {message.content.split()[2]:message.content.split()[3]}
+        bttvemotes.update({message.content.split()[2]:message.content.split()[3]})
+        updatejson=json.dumps(bttvemotes)
+        with open('emotes.json', 'w') as json_file:
+            json.dump(bttvemotes, json_file)
+        await message.channel.send("Emote added")
+
+async def send_emote(message):
+    response=baseulr+bttvemotes[message.content]+"/3x"
+    with open('temp.gif','wb') as f:
+        f.write(requests.get(response).content)
+    await message.channel.send(file=discord.File("temp.gif"))
+    os.remove("temp.gif")
+
+async def remove_emote(message):
+    if message.content.split()[2] in bttvemotes:
+        bttvemotes.pop(message.content.split()[2])
+        updatejson=json.dumps(bttvemotes)
+        with open('emotes.json','w') as json_file:
+            json.dump(bttvemotes,json_file)
+        await message.channel.send('Emote removed')
+    else:
+        await message.channel.send("This emote doesn't exists")
+
+async def emotes_list(message):
+    await message.channel.send("Emotes:")
+    output=''.join((keys +', ')for keys in bttvemotes)
+    await message.channel.send(output)
 
 @client.event
 async def on_ready():
@@ -26,32 +54,21 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if ('Bot' in message.content or 'bot' in message.content) and ('yetki' in message.content or 'Yetki' in message.content):
-        await message.channel.send("Ne zannettin?")
-        response=baseulr+bttvemotes["EZ"]+"/3x"
-        with open('temp.gif','wb') as f:
-            f.write(requests.get(response).content)
-            
-        await message.channel.send(file=discord.File("temp.gif"))
-        os.remove("C:\\Users\Furka\Desktop\DiscordBot/temp.gif")
 
-    if message.content.startswith('bttvadd'):
-        if message.content.split()[1] in bttvemotes:
-            await message.channel.send("This emote already exists")
-        else:
-            updatedic= {message.content.split()[1]:message.content.split()[2]}
-            bttvemotes.update(updatedic)
-            updatejson=json.dumps(bttvemotes)
-            with open('emotes.json', 'w') as json_file:
-                json.dump(bttvemotes, json_file)
-            await message.channel.send("Emote added")
+    if message.content.startswith(prefix):
+        if message.content.split()[1] == 'add':
+            await add_emote(message)
+        if message.content.split()[1] == 'remove':
+            await remove_emote(message)
+        if message.content.split()[1] == 'emotelist':
+            await emotes_list(message)
+        if message.content.split()[1] not in commands:
+            await message.channel.send('That command doesn\'t exist.')
+            await message.channel.send("Commands:")
+            output=''.join((command + ' ')for command in commands)
+            await message.channel.send(output)
 
     if message.content in bttvemotes:
-        response=baseulr+bttvemotes[message.content]+"/3x"
-        with open('temp.gif','wb') as f:
-            f.write(requests.get(response).content)
-            
-        await message.channel.send(file=discord.File("temp.gif"))
-        os.remove("C:\\Users\Furka\Desktop\DiscordBot/temp.gif")
+        await send_emote(message)
 
 client.run(TOKEN)
